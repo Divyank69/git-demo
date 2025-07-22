3a3b2ab0ce182b4025a380a749f0a732086b8c4d
Divyank69 <divyankchoudhary.hp@gmail.com>
2025-07-22 14:40:32 +0530

    added all console.log changes
    add console.log
    
    add console third
    
    fourth commit
    
    fifth commit

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function sayHello() {
   alert("Hello from JavaScript!");
+  console.log("Hello Console!");
+  console.log("third commit");
+  console.log("fourth commit");
+  console.log("fifth commit ");
+
 }
